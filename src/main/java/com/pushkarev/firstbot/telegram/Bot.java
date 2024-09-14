884ca09e2dbebb20f9fcc406cd00307f7ecb794f
pushkarev <pqi2gb@mail.ru>
2024-09-14 18:27:01 +0500

    add dispatcher + handlers

--- src/main/java/com/pushkarev/firstbot/telegram/Bot.java
+++ src/main/java/com/pushkarev/firstbot/telegram/Bot.java
@@ -1,5 +1,6 @@
 package com.pushkarev.firstbot.telegram;
 
+import com.pushkarev.firstbot.service.UpdateDispatcher;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,16 +14,20 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 public class Bot extends TelegramWebhookBot {
 
     final TelegramProperties telegramProperties;
+    final UpdateDispatcher updateDispatcher;
+
 
     @Autowired
-    public Bot(TelegramProperties telegramProperties) {
+    public Bot(TelegramProperties telegramProperties,
+               UpdateDispatcher updateDispatcher) {
         super(telegramProperties.getToken());
         this.telegramProperties = telegramProperties;
+        this.updateDispatcher = updateDispatcher;
     }
 
     @Override
     public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
-        return null;
+        return updateDispatcher.distribute(update, this);
     }
 
     @Override
