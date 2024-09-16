e3033f493045159b03878366fdf9e656b7b9e174
pushkarev <pqi2gb@mail.ru>
2024-09-16 22:00:04 +0500

    - добавлена фабрика - добавлены кнопки

---
+++ src/main/java/com/pushkarev/firstbot/service/data/Command.java
@@ -0,0 +1,8 @@
+package com.pushkarev.firstbot.service.data;
+
+public class Command {
+
+    public static final String START = "/start";
+    public static final String FEEDBACK = "/feedback";
+    public static final String HELP = "/help";
+}
